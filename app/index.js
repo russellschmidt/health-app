b410838826a95c29f8530aeea136d3c1876b066a
Russell Schmidt <russell.schmidt@me.com>
2017-03-16 17:04:13 -0700

    change main message

--- app/index.js
+++ app/index.js
@@ -4,7 +4,7 @@ var ReactDOM = require('react-dom');
 var HelloWorld = React.createClass({
   render: function() {
     return (
-      <div>Health App</div>
+      <div>This is a Health App</div>
     )
   }
 });
